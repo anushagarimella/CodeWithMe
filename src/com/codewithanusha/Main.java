b1996caab27fe5c8193437c53fa6ffb982c81af5
Anusha Garimella <anushagarimella312@gmail.com>
2021-02-05 23:59:29 +1100

    Updated Mortgage calculator

--- src/com/codewithanusha/Main.java
+++ src/com/codewithanusha/Main.java
@@ -1,21 +1,49 @@
 package com.codewithanusha;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
 
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("What is the temperature like today?");
-      int temp = scanner.nextInt();
-        if (temp > 30) {
-            System.out.println("It's a hot day!!");
-            System.out.println("Stay hydrated :) ");
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Principal:") ;
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+                System.out.println("Enter a value between 1000 and 1000000");
         }
-        else if (temp > 20 )
-            System.out.println("Beautiful day to step out for a walk");
-        else
-            System.out.println("It\'s a cold day");
+        while (true) {
+            System.out.println("Annual Interest rate:");
+            float annualInterest=scanner.nextFloat();
+            if(annualInterest>=1&&annualInterest<=30)
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR ;
+            break;
+        }
+        System.out.println("Enter a value between 1 and 30");
+
+        while (true){
+            System.out.println("Period(Years):");
+            byte years=scanner.nextByte();
+            if(years>=1&&years<=30){
+                numberOfPayments=years*MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30 ");
+        }
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format();
+        System.out.println("Mortgage:" + mortgageFormatted);
     }
 }
